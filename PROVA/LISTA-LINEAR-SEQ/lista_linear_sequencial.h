01491b4d84f1f213264ec18e662ec701bf6b93b7
Cibele Arahel Menezes Batista <99154448+cibelearahel@users.noreply.github.com>
2023-06-16 13:00:02 -0300

    u lista_linear_sequencial.h

--- PROVA/LISTA-LINEAR-SEQ/lista_linear_sequencial.h
+++ PROVA/LISTA-LINEAR-SEQ/lista_linear_sequencial.h
@@ -1,1 +1,21 @@
+#ifndef LISTA_LINEAR_SEQUENCIAL_H
+#define LISTA_LINEAR_SEQUENCIAL_H
 
+typedef struct {
+    int* elementos;
+    int tamanho;
+    int capacidade;
+} ListaLinearSequencial;
+
+ListaLinearSequencial* CriarLista(int tamanhoMaximo);
+void DestruirLista(ListaLinearSequencial* lista);
+void InserirElemento(ListaLinearSequencial* lista, int elemento);
+void InserirElementoOrdenado(ListaLinearSequencial* lista, int elemento);
+int ObterQuantidadeElementos(ListaLinearSequencial* lista);
+int BuscarSequencial(ListaLinearSequencial* lista, int elemento);
+int BuscaBinaria(ListaLinearSequencial* lista, int elemento);
+void ExcluirElementoPosicao(ListaLinearSequencial* lista, int posicao);
+void ExcluirElementoValor(ListaLinearSequencial* lista, int elemento);
+void ImprimirElementos(ListaLinearSequencial* lista);
+
+#endif /* LISTA_LINEAR_SEQUENCIAL_H */
